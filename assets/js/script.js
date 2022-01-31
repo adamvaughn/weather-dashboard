12b357a88c19c73473efa1ccba95a34c3723ef5a
adamvaughn <adamdvaughn@gmail.com>
2022-01-30 19:52:42 -0600

    JS variables and api key

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function initPage() {
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+    // API Key
+    const apiKey = "780768745efad4cf67990f3dd59b23b2";
+}
+
+initPage();
